87cfa2fab3940b57140b225efe1ed35ab3d4b863
batiart <117641859+batiart@users.noreply.github.com>
2023-01-05 14:29:03 +0100

    Add files via upload

---
+++ galeria.php
@@ -0,0 +1,17 @@
+
+<html>
+
+<head>
+<title>Batiart Galeria</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta charset="utf-8">
+<link rel="stylesheet" href="galeria.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Abril+Fatface">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans">
+
+</head>
+<body>
+<a href="index.html" class="sticky">BatiART</a>
+<?php include 'gale.php'?>
+</body>
+</html>
